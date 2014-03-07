ce30059f65a988836be6b4da5710dad6441092d8
Eroica <Leye@rapture.fritz.box>
2014-03-07 15:21:41 +0100

    mac patch applied

--- config.def.h
+++ config.def.h
@@ -95,7 +95,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
